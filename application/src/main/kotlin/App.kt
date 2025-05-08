a59d1f4569e5d939bbebe39915b039d128b756c4
ericy <eric.yunsheng.cao@gmail.com>
2025-05-08 19:07:18 -0400

    create gradle project and submodules for python, javafx/kotlin environments

---
+++ application/src/main/kotlin/App.kt
@@ -0,0 +1,9 @@
+import javafx.application.Application
+import javafx.stage.Stage
+
+class App : Application() {
+    override fun start(p0: Stage?) {
+        TODO("Not yet implemented")
+    }
+
+}
